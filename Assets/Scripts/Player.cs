170ba88b418748af186022bc8c8a37988f482b08
Tina <143371814+Trinketina@users.noreply.github.com>
2025-02-08 13:10:57 -0600

    namespace

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,35 +2,38 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+namespace kristina
 {
-    [SerializeField] float speed = 25;
-    Vector3 movement = Vector3.zero;
-
-    Rigidbody rb;
-
-    void Start()
+    public class Player : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-    }
-    void Update()
-    {
-        rb.linearVelocity = movement * Time.deltaTime * speed;
-    }
+        [SerializeField] float speed = 25;
+        Vector3 movement = Vector3.zero;
 
-    public void MoveInput(InputAction.CallbackContext ctx)
-    {
-        //camera directional movement
-        Vector3 camRight = Camera.main.transform.right;
-        camRight.y = 0;
-        camRight.Normalize();
+        Rigidbody rb;
 
-        Vector3 camForward = Camera.main.transform.forward;
-        camForward.y = 0;
-        camForward.Normalize();
-        //--
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        void Update()
+        {
+            rb.linearVelocity = movement * Time.deltaTime * speed;
+        }
 
-        Vector3 input = ctx.ReadValue<Vector2>();
-        movement = (input.x * camRight + input.y * camForward).normalized;
+        public void MoveInput(InputAction.CallbackContext ctx)
+        {
+            //camera directional movement
+            Vector3 camRight = Camera.main.transform.right;
+            camRight.y = 0;
+            camRight.Normalize();
+
+            Vector3 camForward = Camera.main.transform.forward;
+            camForward.y = 0;
+            camForward.Normalize();
+            //--
+
+            Vector3 input = ctx.ReadValue<Vector2>();
+            movement = (input.x * camRight + input.y * camForward).normalized;
+        }
     }
 }
